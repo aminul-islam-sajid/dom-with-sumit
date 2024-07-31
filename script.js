5bd67357b086f8f90cb51b468f4e6e5f545398a6
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-07-31 23:16:14 +0600

    nth-child

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let lestItems = document.querySelectorAll('.item:last-child')
-for(let element of lestItems){
-element.style.color = 'red';
-}
+let lestItems = document.querySelector('#items').querySelector('.item:nth-child(2)')
 
+
+// for(let element of lestItems){
+// element.style.color = 'red';
+// }
+
+lestItems.style.color ='red'
 console.log(lestItems);
