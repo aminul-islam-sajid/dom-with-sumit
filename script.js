1869ff8495e27a7640c7a7014b0773a2e80e49f6
aminul-islam-sajid <85724932+aminul-islam-sajid@users.noreply.github.com>
2024-08-01 11:34:03 +0600

    cheldren

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let lestItems = document.querySelector('#items').querySelector('.item:nth-child(2)')
+// let lestItems = document.querySelector('#items').querySelector('.item:nth-child(2)')
 
 
 // for(let element of lestItems){
 // element.style.color = 'red';
 // }
 
-lestItems.style.color ='red'
-console.log(lestItems);
+// lestItems.style.color ='red'
+// console.log(lestItems);
+
+const parent = document.querySelector('#items')
+const children = parent.children;
+
+console.log(children);
